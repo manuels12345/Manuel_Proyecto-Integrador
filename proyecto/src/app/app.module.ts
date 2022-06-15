8728ca728012a6a986fe2a11387ba35932d70999
manuels12345 <92234244+manuels12345@users.noreply.github.com>
2022-06-14 23:19:05 -0300

    Haciendo header

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SLogosComponent } from './components/s-logos/s-logos.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SLogosComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
---
+++ proyecto/src/app/components/s-logos/s-logos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-s-logos',
+  templateUrl: './s-logos.component.html',
+  styleUrls: ['./s-logos.component.css']
+})
+export class SLogosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
